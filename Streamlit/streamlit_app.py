992eee0f7d01f088c760877e704b208ea391e1b5
Dennis Frank <stu244762@mail.uni-kiel.de>
2026-03-09 16:18:39 +0100

    py update

--- Streamlit/streamlit_app.py
+++ Streamlit/streamlit_app.py
@@ -1,6 +1,18 @@
 import pandas as pd
-import dash
-from dash import dcc
-from dash import html
-from dash import callback, Input, Output
+import streamlit as st
 import plotly.express as px
+
+st.title("Bird observations and its influences")
+
+st.write("Welcome to the website")
+
+st.subheader("Data check")
+
+try:
+   
+    df = pd.read_csv("RQ_1/Hamburg_air_pollution_statistics_2021-2025.csv")
+    st.success("CSV found!")
+    st.write(df.head())
+except:
+    st.error("No CSV found. Please check the file path.")
+   
